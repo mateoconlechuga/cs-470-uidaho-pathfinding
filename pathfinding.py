bf49666b821cce438fbc0f9730990e946490c331
MateoConLechuga <matthewwaltzis@gmail.com>
2018-02-08 19:37:36 -0800

    PyCharm analysis + fixes

--- pathfinding.py
+++ pathfinding.py
@@ -15,12 +15,13 @@
 
 from PIL import Image, ImageDraw
 
-class find(object):
+
+class Find(object):
     def __init__(self, filename=None, style=None, heuristic=None):
         with open(filename) as f:
             data = f.readlines()
 
-        self.costs = {'R' : 1, 'f' : 2, 'F' : 4, 'h' : 5, 'r' : 7, 'M' : 10}
+        self.costs = {'R': 1, 'f': 2, 'F': 4, 'h': 5, 'r': 7, 'M': 10}
         self.width = int(data[0].split()[0])
         self.height = int(data[0].split()[1])
         self.start = tuple(map(int, data[1].split()))
@@ -38,28 +39,34 @@ class find(object):
         self.end = self.style()
         pass
 
-    def test(self, state): return self.goal == state
-    def cost(self, state): return self.costs[self.map[state[1]][state[0]]]
+    def test(self, state):
+        return self.goal == state
+
+    def cost(self, state):
+        return self.costs[self.map[state[1]][state[0]]]
+
     def valid(self, state):
         x, y = state
-        return (x >= 0 and y >= 0 and x < self.width and y < self.height and
-                self.map[y][x] != 'W')
+        return x >= 0 and y >= 0 and x < self.width and y < self.height and self.map[y][x] != 'W'
 
     def expand(self, state):
         x, y = state
         result = list()
-        for neighbor in ((x, y-1), (x, y+1), (x+1, y), (x-1, y)):
-            if self.valid(neighbor): result.append(neighbor)
+        for neighbor in ((x, y - 1), (x, y + 1), (x + 1, y), (x - 1, y)):
+            if self.valid(neighbor):
+                result.append(neighbor)
         return result
 
     def add_fifo(self, state):
         self.openset.append(state)
+
     def add_set(self, state):
         self.openset.add(state)
 
     def take_first(self):
         self.out += 1
         return self.openset[self.out - 1]
+
     def take_sorted(self):
         index = 0
         fsort = self.sort()
@@ -72,8 +79,10 @@ class find(object):
 
     def sort_heuristic_cost(self):
         return sorted(self.f, key=lambda state: self.g[state] + self.heuristic(state))
+
     def sort_heuristic(self):
         return sorted(self.f, key=lambda state: self.heuristic(state))
+
     def sort_cost(self):
         return sorted(self.f, key=lambda state: self.g[state])
 
@@ -83,6 +92,7 @@ class find(object):
         self.add = self.add_fifo
         self.score = self.none
         return self.process()
+
     def lowest_cost(self):
         self.openset = set()
         self.take = self.take_sorted
@@ -90,6 +100,7 @@ class find(object):
         self.sort = self.sort_cost
         self.score = self.cost
         return self.process()
+
     def greedy(self):
         self.openset = set()
         self.take = self.take_sorted
@@ -97,6 +108,7 @@ class find(object):
         self.sort = self.sort_heuristic
         self.score = self.none
         return self.process()
+
     def a_star(self):
         self.openset = set()
         self.take = self.take_sorted
@@ -126,12 +138,15 @@ class find(object):
                         if child not in self.openset:
                             self.add(child)
 
-    def none(self, state):
+    @staticmethod
+    def none(state):
         return 0
+
     def euclidean(self, state):
         x0, y0 = self.goal
         x1, y1 = state
-        return ((x0 - x1)**2 + (y0 - y1)**2)**(1.0/2)
+        return ((x0 - x1) ** 2 + (y0 - y1) ** 2) ** (1.0 / 2)
+
     def manhattan(self, state):
         x0, y0 = self.goal
         x1, y1 = state
@@ -141,42 +156,43 @@ class find(object):
         total = 0
         moves = [self.end]
         state = self.path[self.end]
-        while not state == None:
+        while state is not None:
             moves.append(state)
             total += self.cost(state)
             state = self.path[state]
         moves.reverse()
         return moves, total
 
-def main():
-    types = [   'breadth_first', 'none',
-                'lowest_cost', 'none',
-                'greedy', 'none',
-                'greedy', 'manhattan',
-                'greedy', 'euclidean',
-                'a_star', 'manhattan',
-                'a_star', 'euclidean' ]
 
-    terrain = { 'R' : [128, 128, 128], 'f' : [244, 164, 96],
-                'F' : [85, 107, 47], 'h' : [205, 133, 63],
-                'r' : [0, 0, 128], 'M' : [139, 69, 19],
-                'W' : [25, 25, 110] }
+def main():
+    types = ['breadth_first', 'none',
+             'lowest_cost', 'none',
+             'greedy', 'none',
+             'greedy', 'manhattan',
+             'greedy', 'euclidean',
+             'a_star', 'manhattan',
+             'a_star', 'euclidean']
+
+    terrain = {'R': [128, 128, 128], 'f': [244, 164, 96],
+               'F': [85, 107, 47], 'h': [205, 133, 63],
+               'r': [0, 0, 128], 'M': [139, 69, 19],
+               'W': [25, 25, 110]}
 
     for j in range(0, len(types), 2):
         type_str = types[j]
         if types[j] == 'a_star' or types[j] == 'greedy':
-            type_str += '_' + types[j+1]
+            type_str += '_' + types[j + 1]
 
         try:
-            search = find('map.txt', types[j], types[j+1])
+            search = Find('map.txt', types[j], types[j + 1])
             path, cost = search.get()
-        except:
+        except Exception:
             return print('error: no path found')
 
         offset = 21
         width = search.width * 10 + 1
         height = search.height * 10 + 1
-        img = Image.new('RGB', (width, height + offset), color = 'black')
+        img = Image.new('RGB', (width, height + offset), color='black')
 
         draw = ImageDraw.Draw(img)
         for x in range(search.width + 1):
@@ -198,13 +214,14 @@ def main():
                         draw.rectangle(outline, None, 'yellow')
                     elif (x, y) == search.goal:
                         draw.rectangle(outline, None, 'green')
-            draw.text((2, 0), type_str, (255,255,255))
-            draw.text((2, 10), 'cost: ' + str(cost), (255,255,255))
-            draw.text((width-67, 10), 'length: ' + str(len(path)), (255,255,255))
+            draw.text((2, 0), type_str, (255, 255, 255))
+            draw.text((2, 10), 'cost: ' + str(cost), (255, 255, 255))
+            draw.text((width - 67, 10), 'length: ' + str(len(path)), (255, 255, 255))
 
         print('wrote: ' + type_str + '.png')
         print('cost: ' + str(cost))
         print('length: ' + str(len(path)))
         img.save(type_str + '.png')
+
 
 main()
